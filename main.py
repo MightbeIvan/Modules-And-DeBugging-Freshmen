93451cbd6babe1c279b0dd0535e7e0eadbe6c55b
ivan <ivanjojok@gmail.com>
2025-12-03 11:03:46 -0500

    drills

---
+++ main.py
@@ -0,0 +1,49 @@
+#Fresmen Explore Modules and Debugging Exercise.
+
+#Import any modules down here.
+
+
+
+
+#Q1: use a print statement to print "hello world"
+#use print("")
+
+
+
+
+#Q2: Use the random module to pick a number between 1 and  100
+
+
+
+
+#Q3: use the Cowsay module to make a cow say a sentence of youe choice
+
+
+
+
+
+
+
+ #Q4: Make a different Animal say someting else
+
+
+
+
+
+#Q5:Make a cow say a random number from 1 to 100 (use both modules)
+
+
+
+
+
+
+
+#Debugging try to find and resolve the errors.
+cowsay.cow(Hello world)
+
+prnt(random.randint(0,10)
+
+
+
+#Extra Challenge: Do whatever you want with the modules you have learnt today.
+
